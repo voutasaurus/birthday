841692b487e10c3fdf87cd1c1cfb28a273f68f16
voutasaurus <voutasaurus@gmail.com>
2016-05-25 11:58:31 -0700

    paradox: clarify main terms

--- paradox.go
+++ paradox.go
@@ -22,27 +22,28 @@ func prob(m, n int64) (float64, int) {
 
 	// Probability that the n items all have their own boxes
 	// given m available boxes
-	probability.SetFrac(numerator.MulRange(m-n+1, m), pow(m,n))
-	
+	probability.SetFrac(numerator.MulRange(m-n+1, m), pow(m, n))
+
 	// Probability that there is some overlap somewhere
 	probability.Sub(big.NewRat(1, 1), probability)
-	
+
 	// Floating point version (for display)
 	p, _ := probability.Float64()
-	
+
 	// Check to see whether p is overestimate or underestimate
 	// p > probability: b = -1
 	// p == probability: b = 0
 	// p < probability: b = 1
 	z := new(big.Rat)
 	b := probability.Cmp(z.SetFloat64(p))
-	
-	return p,b
+
+	return p, b
 }
 
 func main() {
-	//fmt.Println(fac(2))
-	//fmt.Println(pow(10, 10))
-	p, b := prob(100000000000,44500)
-	fmt.Println(p, b)
+	holes := int64(3500 * 1000)
+	pidgeons := int64(4000)
+
+	pCollision, rounding := prob(holes, pidgeons)
+	fmt.Println(pCollision, rounding)
 }
