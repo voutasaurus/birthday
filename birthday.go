a7ce620790a88296517fb77d1d3c4d46a43025fd
Shantanu <joshishantanu4@gmail.com>
2016-05-27 18:08:41 -0700

    birthday: compute using log. avoid BigInt.

--- birthday.go
+++ birthday.go
@@ -3,56 +3,28 @@ package main
 import (
 	"flag"
 	"fmt"
-	"math/big"
-	"os"
+	"math"
 )
 
 var (
-	flagHoles   = flag.Int("h", 0, "specify the number of holes available")
-	flagPigeons = flag.Int("p", 0, "specify the number of pigeons to place randomly in holes")
+	flagHoles   = flag.Int64("h", 0, "specify the number of holes available")
+	flagPigeons = flag.Int64("p", 0, "specify the number of pigeons to place randomly in holes")
 )
-
-var sayRounding = map[int]string{
-	-1: " (rounded up)",
-	0:  " (exactly)",
-	1:  " (rounded down)",
-}
 
 func main() {
 	flag.Parse()
-	holes := int64(*flagHoles)
-	pigeons := int64(*flagPigeons)
+	holes := float64(*flagHoles)
+	pigeons := float64(*flagPigeons)
 
-	pCollision, rounding := prob(holes, pigeons)
-	fmt.Print(pCollision)
-	fmt.Fprintf(os.Stderr, "%s\n", sayRounding[rounding])
+	p := prob(holes, pigeons)
+	fmt.Println(p)
 }
 
-func pow(x, y int64) *big.Int {
-	power := new(big.Int)
-	return power.Exp(big.NewInt(x), big.NewInt(y), nil)
-}
-
-func prob(m, n int64) (float64, int) {
-	probability := new(big.Rat)
-	numerator := new(big.Int)
-
-	// Probability that the n items all have their own boxes
-	// given m available boxes
-	probability.SetFrac(numerator.MulRange(m-n+1, m), pow(m, n))
-
-	// Probability that there is some overlap somewhere
-	probability.Sub(big.NewRat(1, 1), probability)
-
-	// Floating point version (for display)
-	p, _ := probability.Float64()
-
-	// Check to see whether p is overestimate or underestimate
-	// p > probability: b = -1
-	// p == probability: b = 0
-	// p < probability: b = 1
-	z := new(big.Rat)
-	b := probability.Cmp(z.SetFloat64(p))
-
-	return p, b
+// prob computes the probability of a collision i:e two pigeons share the same hole.
+// The probability is computed using the closed form solution: p = 1 - (holes -1 / holes)^C(pigeons,2).
+// Which can be rewritten as: p = 1 - 2^(n)(n-1/2)[log(h -1) - log(h)]
+func prob(holes, pigeons float64) float64 {
+	l := math.Log2(holes-1) - math.Log2(holes)
+	l *= pigeons * (pigeons - 1) / 2
+	return 1 - math.Pow(2, l)
 }
